e1d4a52050099ac1783693785369ab7e22d771db
Japhet Elias <japhet9@gmail.com>
2021-11-16 04:15:11 -0500

    added user auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
   end
 
   resources :about, only: [:index]
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  # resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
